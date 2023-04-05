9a6fd4466b194b46728c162b7359515633a04a1e
CSharpMainDotCom <hanxukun67@gmail.com>
2023-04-05 09:14:39 +0800

    keyboard

---
+++ ITyping/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿namespace ITyping;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+	public MainWindow()
+	{
+		InitializeComponent();
+	}
+
+	protected override void OnPreviewKeyDown(KeyEventArgs e)
+	{
+		switch(e.Key)
+		{
+			case Key.Back:
+				inputText.Text = inputText.Text.Substring(0, inputText.Text.Length - 1);
+				
+				break;
+
+			case Key.Q:
+				inputText.Text = inputText.Text + "q";
+				break;
+		}
+		base.OnPreviewKeyDown(e);
+	}
+
+	protected override void OnPreviewKeyUp(KeyEventArgs e)
+	{
+		switch(e.Key)
+		{
+			case Key.Q:
+				
+			break;
+		}
+
+		base.OnPreviewKeyUp(e);
+	}
+}
